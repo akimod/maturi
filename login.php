33925138c35ff665357d2e52f32dc2df1de155bc
akimod <kuroneko.aki1998@gmail.com>
2019-11-09 18:24:31 +0900

    login機能補修
    補修完了
    ログイン基本機能完成

--- login.php
+++ login.php
@@ -11,24 +11,28 @@ else
 $username=$_POST['username'];
 $password=$_POST['password'];
 // Establishing Connection with Server by passing server_name, user_id and password as a parameter
-$connection = mysql_connect("localhost", "root", "1234");
+//$connection = mysql_connect("localhost", "root", 1234);
+//$connection = new pdo("mysql:host=localhost;dbname=db1","root",1234);
+$connection = mysqli_connect("localhost", "root", 1234, "db1");
 // To protect MySQL injection for Security purpose
 $username = stripslashes($username);
 $password = stripslashes($password);
-$username = mysql_real_escape_string($username);
-$password = mysql_real_escape_string($password);
+//$username = mysql_real_escape_string($username);
+$username = mysqli_real_escape_string($connection,$username);
+//$password = mysql_real_escape_string($password);
+$password = mysqli_real_escape_string($connection,$password);
 // Selecting Database
-$db = mysql_select_db("company", $connection);
+//$db = mysql_select_db("db1", $connection);
 // SQL query to fetch information of registerd users and finds user match.
-$query = mysql_query("select * from login where password='$password' AND username='$username'", $connection);
-$rows = mysql_num_rows($query);
+$query = mysqli_query($connection,"select * from login where password='$password' AND username='$username'");
+$rows = mysqli_num_rows($query);
 if ($rows == 1) {
 $_SESSION['login_user']=$username; // Initializing Session
 header("location: profile.php"); // Redirecting To Other Page
 } else {
 $error = "Username or Password is invalid";
 }
-mysql_close($connection); // Closing Connection
+mysqli_close($connection); // Closing Connection
 }
 }
 ?>
